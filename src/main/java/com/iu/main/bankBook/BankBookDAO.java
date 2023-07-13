6829a64a4515263e1eb6ab5c55aebb653d527d1d
Kireua <akrhkdx@google.com>
2023-07-13 15:19:59 +0900

    Init Web

---
+++ src/main/java/com/iu/main/bankBook/BankBookDAO.java
@@ -0,0 +1,135 @@
+package com.iu.main.bankBook;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.iu.main.utill.DbConnector;
+
+public class BankBookDAO {
+	
+	//상품 N개 조회
+	public ArrayList<BankBookDTO> bankBookSearch(String s) throws Exception{
+		ArrayList<BankBookDTO> ar = new ArrayList<BankBookDTO>();
+		Connection con = DbConnector.getConnection();
+		
+		String sql = "SELECT * FROM BANKBOOK WHERE BOOKNAME LIKE ? ORDER BY BOOKNUM DESC";
+		
+		PreparedStatement st = con.prepareStatement(sql);
+		
+		st.setString(1, "%"+s+"%");
+		
+		ResultSet rs = st.executeQuery();
+		
+		while(rs.next()) {
+			BankBookDTO bankBookDTO = new BankBookDTO();
+			bankBookDTO.setBookNum(rs.getLong("BOOKNUM"));
+			bankBookDTO.setBookName(rs.getString("BOOKNAME"));
+			bankBookDTO.setBookRate(rs.getDouble("BOOKRATE"));
+			bankBookDTO.setBookSale(rs.getInt("BOOKSALE"));
+			ar.add(bankBookDTO);
+		}
+		if(ar.size()==0) {
+			System.out.println("찾는 데이터가 없습니다.");
+		}
+
+		
+		DbConnector.disConnect(rs, st, con);
+		return ar;
+	}
+	
+	//상품 N개 조회
+	public ArrayList<BankBookDTO> bankBooklist() throws Exception{
+		ArrayList<BankBookDTO> ar = new ArrayList<BankBookDTO>();
+		
+		//1. DB 연결
+		Connection con = DbConnector.getConnection();
+		//2. query문
+		String sql = "SELECT * FROM BANKBOOK ORDER BY BOOKNUM DESC";
+		
+		PreparedStatement st = con.prepareStatement(sql);
+		
+		//5. 최종 전송 및 결과 처리
+		ResultSet rs = st.executeQuery();
+		
+		while(rs.next()) {
+			BankBookDTO bankBookDTO = new BankBookDTO();
+			bankBookDTO.setBookNum(rs.getLong("BOOKNUM"));
+			bankBookDTO.setBookName(rs.getString("BOOKNAME"));
+			bankBookDTO.setBookRate(rs.getDouble("BOOKRATE"));
+			bankBookDTO.setBookSale(rs.getInt("BOOKSALE"));
+			ar.add(bankBookDTO);
+		}
+		
+		DbConnector.disConnect(rs, st, con);
+		return ar;
+	}
+	
+	//insert
+	//bankBookAdd
+	
+	public int bankBookAdd(BankBookDTO bankBookDTO) throws Exception {
+		
+		//1. DB 연결
+		Connection con = DbConnector.getConnection();
+		//2. 쿼리문 작성
+		String insertsql= "INSERT INTO BANKBOOK VALUES (BANK_SEQ.NEXTVAL,?,?,?)";
+		
+		//3. 쿼리문 미리 전송
+		PreparedStatement st = con.prepareStatement(insertsql);
+		
+		//4.? 값 세팅
+		st.setString(1, bankBookDTO.getBookName());
+		st.setDouble(2, bankBookDTO.getBookRate());
+		st.setInt(3, bankBookDTO.getBookSale());
+		//5. 최종 전송 및 결과 처리
+		int result = st.executeUpdate();
+		
+		DbConnector.disConnect(st, con);
+		return result;
+		
+	}
+	
+	public BankBookDTO bankBookDetail(BankBookDTO bankBookDTO) throws Exception {
+		Connection con = DbConnector.getConnection();
+		String sql= "SELECT * FROM BANKBOOK WHERE BOOKNUM = ?";
+		
+	
+		PreparedStatement st = con.prepareStatement(sql);
+		
+
+		st.setLong(1, bankBookDTO.getBookNum());
+		ResultSet rs = st.executeQuery();
+		
+		//한 줄 읽기
+		if(rs.next()) {
+			bankBookDTO.setBookNum(rs.getLong("BOOKNUM"));
+			bankBookDTO.setBookName(rs.getString("BOOKNAME"));
+			bankBookDTO.setBookRate(rs.getDouble("BOOKRATE"));
+			bankBookDTO.setBookSale(rs.getInt("BOOKSALE"));
+		}else {
+			bankBookDTO=null;
+		}
+		DbConnector.disConnect(rs, st, con);
+		return bankBookDTO;
+	}
+
+	public int bankBookDelete(BankBookDTO bankBookDTO) throws Exception {
+		//1. DB 연동
+		Connection con = DbConnector.getConnection();
+		//2. query문 작성
+		String sql="DELETE BANKBOOK WHERE BOOKNUM=?";
+		//3. 미리 전송
+		PreparedStatement st = con.prepareStatement(sql);
+		//4. ? 세팅
+		st.setLong(1, bankBookDTO.getBookNum());
+		//5. 최종전송 및 결과 처리
+		int result = st.executeUpdate();
+		
+		System.out.println("Db Delete");
+		
+		DbConnector.disConnect(st, con);
+		return result;
+	}
+}
---
+++ src/main/java/com/iu/main/utill/DbConnector.java
@@ -0,0 +1,41 @@
+package com.iu.main.utill;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DbConnector {
+
+	public static Connection getConnection() throws Exception {
+		String user ="user01";
+		String password="user01";
+		String url="jdbc:oracle:thin:@15.164.162.212:1521:xe";
+		String driver="oracle.jdbc.driver.OracleDriver";
+		
+		//1. 드라이버를 메모리에 로딩
+		Class.forName(driver);
+		
+		//2. DB 연결
+		Connection con = DriverManager.getConnection(url, user, password);
+		
+		//reference type인 con이기 때문에 위의 명령어로 주소를 받아오면 성공했다고 볼 수 있음
+		System.out.println(con);
+		
+		return con;
+		
+	}
+	
+	public static void disConnect(ResultSet rs, PreparedStatement st, Connection con )throws Exception {
+		rs.close();
+		st.close();
+		con.close();
+		
+	}
+	
+	public static void disConnect(PreparedStatement st, Connection con) throws Exception {
+		st.close();
+		con.close();
+	}
+}
